fa3548af8edd236a5b7bf2499445301b3a9e0bb9
BornThisWayfitch <90413444+BornThisWayfitch@users.noreply.github.com>
2023-12-25 22:35:18 +0200

    Lab3 rework

--- LAB 3/main.py
+++ LAB 3/main.py
@@ -1,126 +1,101 @@
 class Array3d:
     def __init__(self, dim0, dim1, dim2):
-        # Инициализация экземпляра класса с заданными размерами
+        # Инициализация размеров массива и самого массива данными
         self.dim0 = dim0
         self.dim1 = dim1
         self.dim2 = dim2
-        # Изменение создания массива без использования NumPy
-        self.array = [[[0.0 for _ in range(dim2)] for _ in range(dim1)] for _ in range(dim0)]
+        self.data = [0] * (dim0 * dim1 * dim2)
 
-    def __getitem__(self, indices):
-        # Индексатор для доступа к элементам массива по трехмерным координатам
-        i, j, k = indices
-        return self.array[i][j][k]
+    def __getitem__(self, index):
+        # Перегрузка оператора [] для доступа к элементам массива
+        i, j, k = index
+        return self.data[i * self.dim1 * self.dim2 + j * self.dim2 + k]
 
-    def GetValues0(self, i):
-        # Возвращает срез массива по первой координате (i, .., ..)
-        return [self.array[i][j][:] for j in range(self.dim1)]
+    def __setitem__(self, index, value):
+        # Перегрузка оператора [] для установки значения элемента массива
+        i, j, k = index
+        self.data[i * self.dim1 * self.dim2 + j * self.dim2 + k] = value
 
-    def GetValues1(self, j):
-        # Возвращает срез массива по второй координате (.., j, ..)
-        return [self.array[i][j][:] for i in range(self.dim0)]
+    # Методы для получения срезов данных по разным осям
+    def get_values_0(self, i):
+        return [self.data[i * self.dim1 * self.dim2 + j * self.dim2 + k] for j in range(self.dim1) for k in range(self.dim2)]
 
-    def GetValues2(self, k):
-        # Возвращает срез массива по третьей координате (.., .., k)
-        return [[self.array[i][j][k] for j in range(self.dim1)] for i in range(self.dim0)]
+    def get_values_1(self, j):
+        return [self.data[i * self.dim1 * self.dim2 + j * self.dim2 + k] for i in range(self.dim0) for k in range(self.dim2)]
 
-    def GetValues01(self, i, j):
-        # Возвращает срез массива по первой и второй координатам (i, j, ..)
-        return self.array[i][j][:]
+    def get_values_2(self, k):
+        return [self.data[i * self.dim1 * self.dim2 + j * self.dim2 + k] for i in range(self.dim0) for j in range(self.dim1)]
 
-    def GetValues02(self, i, k):
-        # Возвращает срез массива по первой и третьей координатам (i, .., k)
-        return [self.array[i][j][k] for j in range(self.dim1)]
+    # Методы для получения срезов данных по плоскостям
+    def get_values_01(self, i, j):
+        return [self.data[i * self.dim1 * self.dim2 + j * self.dim2 + k] for k in range(self.dim2)]
 
-    def GetValues12(self, j, k):
-        # Возвращает срез массива по второй и третьей координатам (.., j, k)
-        return [self.array[i][j][k] for i in range(self.dim0)]
+    def get_values_02(self, i, k):
+        return [self.data[i * self.dim1 * self.dim2 + j * self.dim2 + k] for j in range(self.dim1)]
 
-    def SetValues0(self, i, values):
-        # Устанавливает значения в массиве для заданной первой координаты
+    def get_values_12(self, j, k):
+        return [self.data[i * self.dim1 * self.dim2 + j * self.dim2 + k] for i in range(self.dim0)]
+
+    # Методы для установки срезов данных по разным осям
+    def set_values_0(self, i, values):
         for j in range(self.dim1):
             for k in range(self.dim2):
-                self.array[i][j][k] = values[j][k]
+                self.data[i * self.dim1 * self.dim2 + j * self.dim2 + k] = values[j][k]
 
-    def SetValues1(self, j, values):
-        # Устанавливает значения в массиве для заданной второй координаты
+    def set_values_1(self, j, values):
         for i in range(self.dim0):
             for k in range(self.dim2):
-                self.array[i][j][k] = values[i][k]
+                self.data[i * self.dim1 * self.dim2 + j * self.dim2 + k] = values[i][k]
 
-    def SetValues2(self, k, values):
-        # Устанавливает значения в массиве для заданной третьей координаты
+    def set_values_2(self, k, values):
         for i in range(self.dim0):
             for j in range(self.dim1):
-                self.array[i][j][k] = values[i][j]
+                self.data[i * self.dim1 * self.dim2 + j * self.dim2 + k] = values[i][j]
 
-    def SetValues01(self, i, j, values):
-        # Устанавливает значения в массиве для заданных первой и второй координат
+    # Методы для установки срезов данных по плоскостям
+    def set_values_01(self, i, j, values):
         for k in range(self.dim2):
-            self.array[i][j][k] = values[k]
+            self.data[i * self.dim1 * self.dim2 + j * self.dim2 + k] = values[k]
 
-    def SetValues02(self, i, k, values):
-        # Устанавливает значения в массиве для заданных первой и третьей координат
+    def set_values_02(self, i, k, values):
         for j in range(self.dim1):
-            self.array[i][j][k] = values[j]
+            self.data[i * self.dim1 * self.dim2 + j * self.dim2 + k] = values[j]
 
-    def SetValues12(self, j, k, values):
-        # Устанавливает значения в массиве для заданных второй и третьей координат
+    def set_values_12(self, j, k, values):
         for i in range(self.dim0):
-            self.array[i][j][k] = values[i]
+            self.data[i * self.dim1 * self.dim2 + j * self.dim2 + k] = values[i]
 
-    @classmethod
-    def ones(cls, dim0, dim1, dim2):
-        # Метод для создания массива с одинаковыми элементами, заполненными единицами
-        instance = cls(dim0, dim1, dim2)
-        # Изменение создания массива без использования NumPy
-        instance.array = [[[1.0 for _ in range(dim2)] for _ in range(dim1)] for _ in range(dim0)]
-        return instance
+    # Методы для установки единиц, нулей или заполнения массива значением
+    def ones(self):
+        self.data = [1] * (self.dim0 * self.dim1 * self.dim2)
 
-    @classmethod
-    def zeros(cls, dim0, dim1, dim2):
-        # Метод для создания массива с одинаковыми элементами, заполненными нулями
-        return cls(dim0, dim1, dim2)
+    def zeros(self):
+        self.data = [0] * (self.dim0 * self.dim1 * self.dim2)
 
-    @classmethod
-    def fill(cls, dim0, dim1, dim2, value):
-        # Метод для создания массива с одинаковыми элементами, заполненными заданным значением
-        instance = cls(dim0, dim1, dim2)
-        # Изменение создания массива без использования NumPy
-        instance.array = [[[value for _ in range(dim2)] for _ in range(dim1)] for _ in range(dim0)]
-        return instance
+    def fill(self, value):
+        self.data = [value] * (self.dim0 * self.dim1 * self.dim2)
+
+    # Метод для вывода массива в консоль
+    def array_print(self):
+        for i in range(self.dim0):
+            print(f"i = {i}")
+            for j in range(self.dim1):
+                for k in range(self.dim2):
+                    print(self.data[i * self.dim1 * self.dim2 + j * self.dim2 + k], end=" ")
+                print()
+            print()
+
 
 # Пример использования
-# Создаем экземпляр класса Array3d
-my_array = Array3d(3, 4, 5)
+arr = Array3d(2, 3, 4)
 
-# Используем индексатор для доступа к элементам массива
-print(my_array[1, 2, 3])  # Вывод: 0.0
+arr.set_values_0(0, [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+arr.set_values_0(1, [[13, 14, 15, 16], [17, 18, 19, 20], [21, 22, 23, 24]])
 
-# Используем методы для получения срезов массива
-print(my_array.GetValues0(1))
-print(my_array.GetValues1(2))
-print(my_array.GetValues2(3))
-print(my_array.GetValues01(1, 2))
-print(my_array.GetValues02(1, 3))
-print(my_array.GetValues12(2, 3))
+arr.array_print()
 
-# Используем методы для установки значений в массиве
-my_array.SetValues0(1, [[1.0, 1.0, 1.0, 1.0, 1.0] for _ in range(4)])
-my_array.SetValues1(2, [[1.0 for _ in range(5)] for _ in range(3)])
-my_array.SetValues2(3, [[1.0, 1.0, 1.0, 1.0] for _ in range(3)])
-my_array.SetValues01(1, 2, [1.0, 1.0, 1.0, 1.0, 1.0])
-my_array.SetValues02(1, 3, [1.0, 1.0, 1.0, 1.0])
-my_array.SetValues12(2, 3, [1.0, 1.0, 1.0])
+slice = arr.get_values_0(1)
 
-# Используем методы для создания массива с одинаковыми элементами
-ones_array = Array3d.ones(2, 2, 2)
-zeros_array = Array3d.zeros(2, 2, 2)
-fill_array = Array3d.fill(2, 2, 2, 7.0)
-
-# Выводим полученные массивы
-for i in range(2):
-    for j in range(2):
-        print(ones_array[i, j, :])
-        print(zeros_array[i, j, :])
-        print(fill_array[i, j, :])
+for i in slice:
+    print(i, end=" ")
+print()
