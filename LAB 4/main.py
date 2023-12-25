efe7d6e18410879924eb4b2ac4fb605a1875b572
BornThisWayfitch <90413444+BornThisWayfitch@users.noreply.github.com>
2023-12-25 22:34:34 +0200

    Lab4 rework

--- LAB 4/main.py
+++ LAB 4/main.py
@@ -1,69 +1,93 @@
-import keyboard
-import subprocess
+from typing import Callable, Tuple
+from time import sleep
+from collections import deque
+from threading import Thread
 
-# Класс Command, представляющий команду
-class Command:
-    def execute(self):
-        pass
+# Определение типов
+Action = Callable[[], None]
+Keymap = dict[str, Tuple[Action, Action]]
 
-    def display(self):
-        pass
 
-# Класс для команды "Открыть браузер"
-class OpenBrowserCommand(Command):
-    def execute(self):
-        path_to_browser = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
-        subprocess.Popen([path_to_browser])
-
-    def display(self):
-        return 'Открыть браузер'
-
-# Класс виртуальной клавиатуры
-class VirtualKeyboard:
+class Keyboard:
     def __init__(self):
-        self.key_mappings = {}
-        self.history = []
-        self.browser_process = None
+        # Инициализация словаря для отображения клавиш на действия и истории действий
+        self.keymap: Keymap = {}
+        self.history = deque()
 
-    def remap_key(self, key, command):
-        self.key_mappings[key] = command
+    def reg_key(self, key: str, action: Action, undo_action: Action = lambda: None):
+        # Регистрация клавиши с соответствующими действиями и действиями отмены
+        self.keymap[key] = (action, undo_action)
 
-    def press_key(self, key):
-        if key in self.key_mappings:
-            command = self.key_mappings[key]
-            print(f'Выполнено действие: {command.display()}')
-            self.history.append(command)
-            command.execute()
-        else:
-            print(f'Нажата клавиша: {key}')
+    def press_key(self, key: str):
+        # Обработка нажатия клавиши, проверка наличия клавиши в словаре
+        assert key in self.keymap, "Нажата неизвестная кнопка"
+        # Добавление клавиши в историю и выполнение соответствующего действия
+        self.history.append(key)
+        self.keymap[key][0]()
 
-    def undo_last_action(self):
+    def undo(self):
+        # Отмена последнего действия
         if self.history:
-            undone_command = self.history.pop()
-            print(f'Отменено действие: {undone_command.display()}')
-            undone_command.execute()
-        else:
-            print('История пуста, отмена невозможна')
+            # Получение последней клавиши из истории и выполнение соответствующего действия отмены
+            self.keymap[self.history.pop()][1]()
 
-# Функция-обработчик событий нажатия клавиш
-def on_key_press(event):
-    if event.event_type == keyboard.KEY_DOWN:
-        if event.name == 'ctrl' or event.name == 'z':
-            vk.undo_last_action()
-    else:
-        vk.press_key(event.name)
 
-# Функция для демонстрации работы виртуальной клавиатуры
-def demo_workflow(virtual_keyboard):
-    open_browser_command = OpenBrowserCommand()
-    virtual_keyboard.remap_key('A', open_browser_command)
+class Workflow:
+    def __init__(self, keyboard: Keyboard):
+        # Инициализация объекта Workflow с переданным объектом Keyboard
+        self.keyboard = keyboard
+        self.actions = []
 
-    print("Нажмите клавишу 'A' для открытия браузера, 'Ctrl+Z' для отмены и 'Esc' для завершения")
+    def press(self, key: str):
+        # Метод для имитации нажатия клавиши с задержкой в полсекунды
+        self.keyboard.press_key(key)
+        sleep(0.5)
 
-    keyboard.hook(on_key_press)
-    keyboard.wait('esc')
+    def undo(self):
+        # Метод для имитации отмены действия с задержкой в полсекунды
+        self.keyboard.undo()
+        sleep(0.5)
 
-# Точка входа в программу
+    def start(self):
+        # Метод для запуска последовательности действий Workflow
+        for action in self.actions:
+            action()
+
+
+def main():
+    # Устанавливаем локаль для корректного отображения русского текста
+    # (по аналогии с setlocale(LC_ALL, "Russian") в C++)
+    print("Привет!")
+
+    # Создаем объект Keyboard
+    keyboard = Keyboard()
+
+    # Регистрируем клавиши с соответствующими действиями
+    keyboard.reg_key("A", lambda: print("Нажали A"), lambda: print("Отжали A"))
+    keyboard.reg_key("Alt+F4", lambda: print("Закрыли приложение"), lambda: print("Обратно открыли приложение"))
+
+    # Создаем объект Workflow с переданным объектом Keyboard
+    workflow = Workflow(keyboard)
+
+    # Добавляем последовательность действий в Workflow
+    workflow.actions.extend([
+        lambda: workflow.press("A"),
+        lambda: workflow.press("Alt+F4"),
+        lambda: workflow.undo(),
+        lambda: workflow.undo()
+    ])
+
+    # Запускаем Workflow
+    workflow.start()
+    print()
+
+    # Регистрируем новые действия для клавиш
+    keyboard.reg_key("A", lambda: print("Нажали B"), lambda: print("Отжали B"))
+    keyboard.reg_key("Alt+F4", lambda: print("Теперь это сочетание сдаёт лабораторную"), lambda: print("Больше не сдаёт"))
+
+    # Запускаем Workflow с новыми действиями
+    workflow.start()
+
+
 if __name__ == "__main__":
-    vk = VirtualKeyboard()
-    demo_workflow(vk)
+    main()
